f64b5aba06f8f3f9c6918637674789c6c4a9046e
Brandon <brandonbgrayson@gmail.com>
2021-08-21 19:33:23 -0400

    added signInForm FormGroup and sent data to parent component

--- dialog/dialog.component.ts
+++ dialog/dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup } from '@angular/forms';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog',
@@ -7,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
+  signInForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    
+  ) {
+    this.signInForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName : new FormControl(),
+      password: new FormControl(),
+      email : new FormControl()
+    })
+   }
 
   ngOnInit(): void {
+    this.signInForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName : new FormControl(),
+      password: new FormControl(),
+      email : new FormControl()
+    })
   }
 
+  submit() {
+    console.log("Saving User Input Data!!!")
+    this.dialogRef.close(this.signInForm.value)
+  }
 }
